2ae458a71456084bc3922227e7697662d8d30ad4
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-05-29 19:17:17 +0530

    feat: Add swagger code - authRouter

--- router/authRouter.js
+++ router/authRouter.js
@@ -2,9 +2,43 @@ const express = require("express");
 const AuthRouter = express.Router();
 const utilsController = require("../controller/utilsController");
 
-// AuthRouter.post("/register", )
 AuthRouter.get("/status", utilsController.status);
 AuthRouter.post("/oauth", utilsController.oauth);
+
+/** Auth Methods */
+/**
+ * @openapi
+ * '/auth/register':
+ *  post:
+ *     tags:
+ *     - Auth Controller
+ *     summary: Create a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - username
+ *              - email
+ *            properties:
+ *              username:
+ *                type: string
+ *                default: iamakbarsha1
+ *              email:
+ *                type: string
+ *                default: iamakbarsha1@mail.com
+ *     responses:
+ *      201:
+ *        description: Created
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 AuthRouter.post("/register", utilsController.register);
 
 module.exports = AuthRouter;
